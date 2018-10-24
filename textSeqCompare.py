f05ed0da5fbd7f4c109a19ee38db3ac35af53a84
unknown <timdereuse@gmail.com>
2018-10-24 18:30:57 -0400

    asymmetric gap penalties

--- textSeqCompare.py
+++ textSeqCompare.py
@@ -11,24 +11,26 @@ def read_file(fname):
 
 
 # scoring system
-match = 3
-mismatch = -3
+match = 5
+mismatch = -5
 gap_open = -3
-gap_extend = -1
+gap_extend = -3
+
+gap_open_x = -10
+gap_extend_x = -5
+gap_open_y = -5
+gap_extend_y = -2
 
 # display length
-line_len = 80
+line_len = 90
 
 if __name__ == '__main__':
     files = ['einsiedeln_001r', 'einsiedeln_001v', 'einsiedeln_002r',
         'einsiedeln_002v', 'einsiedeln_003r', 'einsiedeln_003v']
 
-    item = files[4]
+    item = files[5]
     transcript = read_file('./txt/' + item + '_transcript.txt')
     ocr = read_file('./txt/' + item + '_ocr.txt')
-
-    # transcript = 'qwertyuiopasdfghjklzxcvbnmqwertyuiopmasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm'
-    # ocr = 'qwertyuiopasdfgbnmsqswserhjklzxcvtyuiopasmmqwdfghjklzxcvbnertyuiopasdfghjklzxcvbnm'
 
     # y_mat and x_mat keep track of gaps in horizontal and vertical directions
     mat = np.zeros((len(transcript), len(ocr)))
@@ -50,21 +52,24 @@ if __name__ == '__main__':
     for i in range(1, len(transcript)):
         for j in range(1, len(ocr)):
 
+            # update main matrix (for matches)
             match_score = match if transcript[i-1] == ocr[j-1] else mismatch
             mat_vals = [mat[i-1][j-1], x_mat[i-1][j-1], y_mat[i-1][j-1]]
             mat[i][j] = max(mat_vals) + match_score
             mat_ptr[i][j] = int(mat_vals.index(max(mat_vals)))
 
-            y_mat_vals = [mat[i][j-1] + gap_open + gap_extend,
-                        x_mat[i][j-1] + gap_open + gap_extend,
-                        y_mat[i][j-1] + gap_extend]
+            # update matrix for y gaps
+            y_mat_vals = [mat[i][j-1] + gap_open_y + gap_extend_y,
+                        x_mat[i][j-1] + gap_open_y + gap_extend_y,
+                        y_mat[i][j-1] + gap_extend_y]
 
             y_mat[i][j] = max(y_mat_vals)
             y_mat_ptr[i][j] = int(y_mat_vals.index(max(y_mat_vals)))
 
-            x_mat_vals = [mat[i-1][j] + gap_open + gap_extend,
-                        x_mat[i-1][j] + gap_extend,
-                        y_mat[i-1][j] + gap_open + gap_extend]
+            # update matrix for x gaps
+            x_mat_vals = [mat[i-1][j] + gap_open_x + gap_extend_x,
+                        x_mat[i-1][j] + gap_extend_x,
+                        y_mat[i-1][j] + gap_open_x + gap_extend_x]
 
             x_mat[i][j] = max(x_mat_vals)
             x_mat_ptr[i][j] = x_mat_vals.index(max(x_mat_vals))
@@ -88,7 +93,7 @@ if __name__ == '__main__':
             ocr_align += ocr[ypt]
             align_record += 'O' if(transcript[xpt] == ocr[ypt]) else 'X'
 
-            direction = mat_ptr[xpt][ypt]
+            mpt = mat_ptr[xpt][ypt]
             xpt -= 1
             ypt -= 1
 
@@ -96,18 +101,17 @@ if __name__ == '__main__':
             tra_align += transcript[xpt]
             ocr_align += '_'
             align_record += ' '
-            direction = x_mat_ptr[xpt][ypt]
+            mpt = x_mat_ptr[xpt][ypt]
             xpt -= 1
 
         elif mpt == 2:
             tra_align += '_'
             ocr_align += ocr[ypt]
             align_record += ' '
-            direction = y_mat_ptr[xpt][ypt]
+            mpt = y_mat_ptr[xpt][ypt]
             ypt -= 1
 
-        mpt = direction
-        print(mpt, xpt, ypt)
+        # print(mpt, xpt, ypt)
 
     tra_align = tra_align[::-1]
     ocr_align = ocr_align[::-1]
@@ -118,8 +122,7 @@ if __name__ == '__main__':
         end = (n + 1) * line_len
         print(tra_align[start:end])
         print(ocr_align[start:end])
-        print(align_record[start:end])
-        print('\n')
+        print(align_record[start:end] + '\n')
 
     # plt.imshow(x_mat_ptr[1:200, 1:200])
     # plt.colorbar()
